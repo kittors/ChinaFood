2ad668d84a92149bc41f568781d9c8461c6cd663
paidaxing <576560107@qq.com>
2021-12-17 16:20:13 +0800

    首页更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <my-header />
-    <van-tabs v-model="active" v-if="data">
-      <van-tab :title="item" v-for="(item, index) in data" :key="index">
-        <van-grid :column-num="2">
-          <van-grid-item
-            v-for="value in 20"
-            :key="value"
-            icon="photo-o"
-            :text="item"
-          />
+    <van-tabs v-model="active" v-if="data" @click="onClick">
+      <van-tab
+        :title="item.category_name"
+        v-for="(item, index) in data"
+        :key="index"
+      >
+        <van-grid :column-num="2" :gutter="5">
+          <van-grid-item v-for="(item, index) in categorySearch" :key="index">
+            <!-- <van-image :src="require('/public/food/6.jpg')" /> -->
+            <!-- <van-image :src="require(`${item.dishes_pic}`)" /> -->
+            <van-image :src="require('/public/food/' + `${item.dishes_pic}`)" />
+            <span>{{ item.dishes_name }}</span>
+          </van-grid-item>
         </van-grid>
       </van-tab>
     </van-tabs>
@@ -23,32 +27,45 @@ export default {
   data() {
     return {
       active: 0,
-      data: [
-        "推荐",
-        "川菜",
-        "鲁菜",
-        "徽菜",
-        "粤菜",
-        "浙江菜",
-        "江苏菜",
-        "早餐",
-        "晚餐",
-        "午餐",
-        "糕点",
-      ],
+      data: "",
+      categorySearch: "",
     }
   },
   methods: {
     ReqFoodCategory() {
       this.axios.get("/foodCategory").then((res) => {
+        // console.log(res)
+        // console.log(res.data.result)
+        this.data = res.data.result
+      })
+    },
+    // 查询对应title下的菜品
+    onClick(name, title) {
+      console.log(name, title)
+      let url = `categorySearch?category_name=${title}`
+      this.axios.get(url).then((res) => {
         console.log(res)
+        // console.log(res.data.result)
+        this.categorySearch = res.data.result
       })
     },
   },
   mounted() {
     this.ReqFoodCategory()
+    this.onClick()
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  width: 45.5vw;
+  height: 33vh;
+  background-color: antiquewhite;
+  display: inline-block;
+  margin: 3vh 0 0 3vw;
+  img {
+    width: 100%;
+  }
+}
+</style>
