7fc7f3554f75b7f968895156f2fe77a8f8c6af29
kittors <364299311@qq.com>
2021-12-21 19:20:16 +0800

    修改了注册登录的页面样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="tab">
-    <div class="title">用户登录</div>
+  <div class="tab" @click="check">
+    <div class="title">一起发现美食</div>
     <div>
       <van-form validate-first @failed="onFailed" @submit="onSubmit">
         <!-- 通过 pattern 进行正则校验 -->
@@ -8,23 +8,21 @@
           v-model="username"
           name="用户名"
           label="用户名"
-          placeholder="用户名"
+          :placeholder="pu"
           :rules="[{ required: true, message: '请填写用户名' }]"
+          ref="input_user"
+          class="user"
         />
         <van-field
           v-model="pwd"
           type="password"
           name="密码"
           label="密码"
-          placeholder="密码"
+          :placeholder="pd"
           :rules="[{ required: true, message: '请填写密码' }]"
+          class="pd"
+          ref="input_pd"
         />
-        <van-checkbox v-model="checked" style="margin-top: 20px">
-          我已阅读并同意
-          <router-link to="/">用户协议</router-link>
-          和
-          <router-link to="/">隐私政策</router-link>
-        </van-checkbox>
         <div style="margin: 16px">
           <van-button
             round
@@ -50,6 +48,12 @@
         <van-button round block to="/register"> 注册 </van-button>
       </div>
     </div>
+    <van-checkbox v-model="checked" style="margin-top: 40px; font-size: 12px">
+      我已阅读并同意
+      <router-link to="#">用户协议</router-link>
+      和
+      <router-link to="#">隐私政策</router-link>
+    </van-checkbox>
   </div>
 </template>
 
@@ -61,6 +65,8 @@ export default {
       username: "",
       pwd: "",
       password: /^(\w){6,20}$/, //只能输入6-20个字母、数字、下划线
+      pd: "密码",
+      pu: "用户名",
     };
   },
   methods: {
@@ -79,13 +85,27 @@ export default {
         }
       });
     },
-  },
-  created() {
-    console.log(this.$emit);
-    this.$emit("footer", false);
+    check() {
+      // 为了删除placeholder的内容
+      let reg = /^((?!error).)*$/;
+      let u_string = this.$refs.input_user.$el.className;
+      let bool = reg.test(`${u_string}`);
+      if (!bool) this.pu = "";
+      let pd_string = this.$refs.input_pd.$el.className;
+      let bool2 = reg.test(`${pd_string}`);
+      if (!bool2) this.pd = "";
+    },
   },
 };
 </script>
+<style>
+/* 警告文字的样式 */
+div.van-field__error-message {
+  position: absolute;
+  top: 0;
+  font-size: 14px;
+}
+</style>
 
 <style lang="scss" scoped>
 .tab {
@@ -96,9 +116,11 @@ export default {
   flex-flow: column;
   justify-content: center;
   .title {
-    font-size: 32px;
+    font-size: 28px;
+    position: fixed;
     font-weight: bold;
     text-align: center;
+    top: 50px;
   }
   .phone {
     border: 1px solid #ebedf0;
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tab">
+  <div class="tab" @click="check">
     <div class="title">用户注册</div>
     <div>
       <van-form validate-first @failed="onFailed" @submit="onSubmit">
@@ -9,16 +9,18 @@
           v-model="username"
           name="用户名"
           label="用户名"
-          placeholder="用户名"
+          :placeholder="pu"
           :rules="[{ required: true, message: '请填写用户名' }]"
+          ref="input_user"
         />
         <van-field
           class="email"
           v-model="email"
           name="pattern"
           label="邮箱"
-          placeholder="请输入邮箱"
+          :placeholder="pm"
           :rules="[{ pattern, message: '请输入正确邮箱' }]"
+          ref="input_email"
         />
         <van-field
           class="password"
@@ -38,12 +40,6 @@
           placeholder="请再次输入密码"
           :rules="[{ validator, message: '密码与上面不一致,请重新输入密码' }]"
         />
-        <van-checkbox v-model="checked" style="margin-top: 20px">
-          我已阅读并同意
-          <router-link to="/">用户协议</router-link>
-          和
-          <router-link to="/">隐私政策</router-link>
-        </van-checkbox>
         <div style="margin: 16px">
           <van-button
             round
@@ -69,6 +65,12 @@
         <van-button round block to="/login"> 登录 </van-button>
       </div>
     </div>
+    <van-checkbox v-model="checked" style="margin-top: 40px; font-size: 12px">
+      我已阅读并同意
+      <router-link to="/">用户协议</router-link>
+      和
+      <router-link to="/">隐私政策</router-link>
+    </van-checkbox>
   </div>
 </template>
 
@@ -81,38 +83,59 @@ export default {
       pwd: "",
       yz_pwd: "",
       email: "",
+      pu: "用户名",
+      pm: "请输入邮箱",
       pattern:
         /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/, //邮箱正则验证
       password: /^(\w){6,20}$/, //只能输入6-20个字母、数字、下划线
-    }
+    };
   },
   methods: {
     onFailed(errorInfo) {
-      console.log("failed", errorInfo)
+      console.log("failed", errorInfo);
     },
     // 校验函数返回 true 表示校验通过，false 表示不通过
     validator(val) {
       if (this.value3 == this.value2) {
-        return true
+        return true;
       } else {
-        return false
+        return false;
       }
     },
     onSubmit(values) {
       // console.log(values)
-      let params = `email=${this.email}&password=${this.pwd}&username=${this.username}`
+      let params = `email=${this.email}&password=${this.pwd}&username=${this.username}`;
       this.axios.post("/register", params).then((res) => {
         // console.log(res)
         if (res.data.code == 200) {
-          console.log("登录成功")
+          console.log("登录成功");
         } else {
-          console.log("登录失败")
+          console.log("登录失败");
         }
-      })
+      });
+    },
+    check() {
+      // 为了删除placeholder的内容
+      let reg = /^((?!error).)*$/;
+      let u_string = this.$refs.input_user.$el.className;
+      let bool = reg.test(`${u_string}`);
+      if (!bool) this.pu = "";
+      let em_string = this.$refs.input_email.$el.className;
+      let bool2 = reg.test(`${em_string}`);
+      if (!bool2) this.pm = "";
     },
   },
-}
+};
 </script>
+
+<style>
+/* 警告文字的样式 */
+div.van-field__error-message {
+  position: absolute;
+  top: 0;
+  font-size: 14px;
+}
+</style>
 
 <style lang="scss" scoped>
 .tab {
@@ -123,9 +146,11 @@ export default {
   flex-flow: column;
   justify-content: center;
   .title {
-    font-size: 32px;
+    font-size: 28px;
+    position: fixed;
     font-weight: bold;
     text-align: center;
+    top: 50px;
   }
   .username,
   .email,
